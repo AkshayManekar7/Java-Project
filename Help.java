0e0a56395d8a46bafceeb2e2209d95551b3be6e3
AkshayManekar7 <akshaymanekar18@gmail.com>
2024-07-30 21:15:52 +0530

    first commit

---
+++ Help.java
@@ -0,0 +1,8 @@
+// This is a simple Java program.
+class HelloWorld {
+    // Your program begins with a call to main().
+    // Prints "Hello" to the terminal window.
+    public static void main(String args[]) {
+        System.out.println("Hello");
+    }
+}
